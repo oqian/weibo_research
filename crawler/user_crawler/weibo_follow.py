42bafd829099686f53abef6e43ad439cff70305d
Qiyi Shan <adls371@outlook.com>
2021-07-22 01:58:10 -0700

    Massive refactor. Fixed user ID repeating issue. Add progress bar

--- crawler/user_crawler/weibo_follow.py
+++ crawler/user_crawler/weibo_follow.py
@@ -10,16 +10,40 @@ import requests
 from time import sleep
 from lxml import etree
 from tqdm import tqdm
-from typing import Dict
+from typing import Dict, Set, Tuple, List
+
+# Type alias for readability
+UserId = str
+CrawlDepth = int
 
 
-class Follow(object):
-    def __init__(self, config):
-        """Follow类初始化"""
-        self.cookie = config['cookie']
-        self.user_id_list = config['user_id_list']
+def random_sleep():
+    if random.random() < 0.4:
+        sleep(random.randint(3, 5))
+
+
+def write_to_txt(follow_list):
+    with open('user_id_list.txt', 'ab') as f:
+        for user in follow_list:
+            f.write((user['uri'] + ' ' + user['nickname'] + '\n').encode(
+                sys.stdout.encoding))
+
+
+class WeiboUserCrawler:
+    def __init__(self, cookie: str, crawl_queue: List[Tuple[UserId, CrawlDepth]], max_depth: CrawlDepth = 0,
+                 visited_user_id_set: Set[UserId] = None):
+        self.cookie = cookie
+        self.crawl_queue = crawl_queue
+        self.visited_user_id_set = visited_user_id_set if visited_user_id_set else set()
+        self.max_depth = max_depth
         self.user_id = ''
         self.follow_list = []  # 存储爬取到的所有关注微博的uri和用户昵称
+
+    @classmethod
+    def init_from_config(cls, config: Dict, max_depth=0):
+        user_id_depth_queue = [(user_id, 0) for user_id in config["user_id_list"]]
+        return WeiboUserCrawler(cookie=config["cookie"], crawl_queue=user_id_depth_queue,
+                                max_depth=max_depth)
 
     def query_webpage(self, url):
         """处理html"""
@@ -33,9 +57,9 @@ class Follow(object):
             print('Error: ', e)
             traceback.print_exc()
 
-    def get_page_num(self):
+    def crawl_page_count(self, user_id: UserId):
         """获取关注列表页数"""
-        url = "https://weibo.cn/%s/fans" % self.user_id
+        url = "https://weibo.cn/%s/fans" % user_id
         selector = self.query_webpage(url)
         if selector.xpath("//input[@name='mp']") == []:
             page_num = 1
@@ -44,10 +68,9 @@ class Follow(object):
                 selector.xpath("//input[@name='mp']")[0].attrib['value'])
         return page_num
 
-    def get_one_page(self, page):
+    def crawl_users_from_follower_page(self, user_id: UserId, page: int):
         """获取第page页的user_id"""
-        print(u'%s第%d页%s' % ('-' * 30, page, '-' * 30))
-        url = 'https://weibo.cn/%s/fans?page=%d' % (self.user_id, page)
+        url = 'https://weibo.cn/%s/fans?page=%d' % (user_id, page)
         selector = self.query_webpage(url)
         table_list = selector.xpath('//table')
         if (page == 1 and len(table_list) == 0):
@@ -57,67 +80,36 @@ class Follow(object):
                 im = t.xpath('.//a/@href')[-1]
                 uri = im.split('uid=')[-1].split('&')[0].split('/')[-1]
                 nickname = t.xpath('.//a/text()')[0]
-                if {'uri': uri, 'nickname': nickname} not in self.follow_list:
-                    self.follow_list.append({'uri': uri, 'nickname': nickname})
-                    print(u'%s %s' % (nickname, uri))
+                yield {'uri': uri, 'nickname': nickname}
 
-    def get_follow_list(self):
+    def get_page_list_for_user(self, user_id):
         """获取关注用户主页地址"""
-        page_num = self.get_page_num()
-        print(u'用户关注页数：' + str(page_num))
-        page1 = 0
-        random_pages = random.randint(1, 5)
-        for page in tqdm(range(1, page_num + 1), desc=u'关注列表爬取进度'):
-            self.get_one_page(page)
+        page_count = self.crawl_page_count(user_id)
+        return [page for page in range(1, page_count + 1)]
 
-            if page - page1 == random_pages and page < page_num:
-                sleep(random.randint(6, 10))
-                page1 = page
-                random_pages = random.randint(1, 5)
-
-        print(u'用户关注列表爬取完毕')
-
-    def write_to_txt(self):
-        with open('user_id_list.txt', 'ab') as f:
-            for user in self.follow_list:
-                f.write((user['uri'] + ' ' + user['nickname'] + '\n').encode(
-                    sys.stdout.encoding))
-
-    def get_user_list(self, file_name):
-        """获取文件中的微博id信息"""
-        with open(file_name, 'rb') as f:
-            try:
-                lines = f.read().splitlines()
-                lines = [line.decode('utf-8-sig') for line in lines]
-            except UnicodeDecodeError:
-                sys.exit(u'%s文件应为utf-8编码，请先将文件编码转为utf-8再运行程序' % file_name)
-            user_id_list = []
-            for line in lines:
-                info = line.split(' ')
-                if len(info) > 0 and info[0].isdigit():
-                    user_id = info[0]
-                    if user_id not in user_id_list:
-                        user_id_list.append(user_id)
-        return user_id_list
-
-    def initialize_info(self, user_id):
-        """初始化爬虫信息"""
-        self.follow_list = []
-        self.user_id = user_id
+    def get_expected_user_count_left(self):
+        """Returns expected number of users left to crawl, assuming that an average user has 200 follower. """
+        return sum(map(lambda x: 200 ** (self.max_depth - x[1]), self.crawl_queue))
 
     def start(self):
         """运行爬虫"""
-        try:
-            for user_id in self.user_id_list:
-                self.initialize_info(user_id)
-                print('*' * 100)
-                self.get_follow_list()  # 爬取微博信息
-                self.write_to_txt()
-                print(u'信息抓取完毕')
-                print('*' * 100)
-        except Exception as e:
-            print('Error: ', e)
-            traceback.print_exc()
+        expected_user_count_to_crawl = self.get_expected_user_count_left()
+        time_bar = tqdm(total=expected_user_count_to_crawl, desc=u"（预计）用户爬取数", unit=u"人")
+        while len(self.crawl_queue) > 0:
+            user_id, depth = self.crawl_queue.pop()
+            user_list_to_write = []
+            for page in tqdm(self.get_page_list_for_user(user_id), desc="页数"):
+                random_sleep()
+                for follower_info in self.crawl_users_from_follower_page(user_id, page):
+                    follower_user_id = follower_info["uri"]
+                    if follower_user_id not in self.visited_user_id_set:
+                        if depth < self.max_depth:
+                            self.crawl_queue.insert(0, (follower_user_id, depth + 1))
+                        user_list_to_write.append(follower_info)
+                        self.visited_user_id_set.add(follower_user_id)
+            time_bar.update(expected_user_count_to_crawl - self.get_expected_user_count_left())
+            write_to_txt(user_list_to_write)
+        print(u'信息抓取完毕')
 
 
 class ConfigFileReader:
@@ -176,13 +168,9 @@ class ConfigFileReader:
 
 
 def main():
-    try:
-        config = ConfigFileReader().read()
-        wb = Follow(config)
-        wb.start()  # 爬取微博信息
-    except Exception as e:
-        print('Error: ', e)
-        traceback.print_exc()
+    config = ConfigFileReader().read()
+    wb = WeiboUserCrawler.init_from_config(config, max_depth=0)
+    wb.start()  # 爬取微博信息
 
 
 if __name__ == '__main__':
